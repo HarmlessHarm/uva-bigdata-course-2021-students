1cac4ad2e32e73a74fa39c213d08bbc20558f1ba
Harm <harmmanders@gmail.com>
2021-02-24 13:37:03 +0100

    Task1 done

--- src/main/java/nl/uva/bigdata/hadoop/assignment1/BooksWritable.java
+++ src/main/java/nl/uva/bigdata/hadoop/assignment1/BooksWritable.java
@@ -15,14 +15,52 @@ public class BooksWritable  implements Writable {
         this.books = books;
     }
 
+    /*
+     * write
+     * Write function that writes the Book array to a DataOutput buffer.
+     * Data structure:
+     *      <int>number_of_books
+     *      <String>title_book_1 <int>year_book_1
+     *      <String>title_book_2 <int>year_book_2
+     *      etc.
+     */
     @Override
     public void write(DataOutput out) throws IOException {
-        // TODO Implement me
+        // Write number of books to buffer
+        out.writeInt(books.length);
+
+        // For each book write title as UTF and year as int
+        for (int i=0; i < books.length; i++) {
+            out.writeUTF(books[i].getTitle());
+            out.writeInt(books[i].getYear());
+        }
+
     }
 
+    /*
+     * readFields
+     * Read function that reads an InputOutput buffer and stores it as a Book array.
+     * Expected data structure:
+     *      <int>number_of_books
+     *      <String>title_book_1 <int>year_book_1
+     *      <String>title_book_2 <int>year_book_2
+     *      etc.
+     */
     @Override
     public void readFields(DataInput in) throws IOException {
-        // TODO Implement me
+        // Read number of books from buffer
+        int number = in.readInt();
+        // Create a new Book array
+        Book[] new_books = new Book[number];
+
+        // For each book read title as UTF and year as int
+        for (int i = 0; i < number; i++) {
+            String title = in.readUTF();
+            int year = in.readInt();
+            new_books[i] = new Book(title, year);
+        }
+        // Update this.books using setBooks()
+        this.setBooks(new_books);
     }
 
     @Override
